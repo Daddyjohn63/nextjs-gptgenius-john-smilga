f25e1e5c88a2e9287696297948ea44591a086a22
Daddyjohn63 <john@webandprosper.co.uk>
2023-12-14 08:41:49 +0000

    prior to clerk upgrade

--- app/(dashboard)/tours/[id]/page.js
+++ app/(dashboard)/tours/[id]/page.js
@@ -1,7 +1,10 @@
 import TourInfo from '@/components/TourInfo';
-import { getSingleTour } from '@/utils/action';
+import { generateTourImage, getSingleTour } from '@/utils/action';
+import axios from 'axios';
+import Image from 'next/image';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
+const url = `https://api.unsplash.com/search/photos?client_id=${process.env.UNSPLASH_API_KEY}&query=`;
 
 const SingleTourPage = async ({ params }) => {
   const tour = await getSingleTour(params.id);
@@ -9,11 +12,34 @@ const SingleTourPage = async ({ params }) => {
   if (!tour) {
     redirect('/tours');
   }
+
+  const { data } = await axios.get(`${url}${tour.city}`);
+  const tourImage = data?.results[0]?.urls?.raw;
+
+  //using openai image generation
+
+  // const tourImage = await generateTourImage({
+  //   city: tour.city,
+  //   country: tour.country
+  // });
+
   return (
     <div>
       <Link href="/tours" className="btn btn-secondary mb-12">
         back to tours{' '}
       </Link>
+      {tourImage ? (
+        <div>
+          <Image
+            src={tourImage}
+            width={300}
+            height={300}
+            className="rounded-xl shadow-xl mb-16 h-96 w-96 object-cover"
+            alt={tour.title}
+            priority
+          />
+        </div>
+      ) : null}
       <TourInfo tour={tour} />
     </div>
   );
